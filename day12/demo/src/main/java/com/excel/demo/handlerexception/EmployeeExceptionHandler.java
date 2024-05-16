53e75ed6fc486e80bb1ccad5c5446327e0e00a78
Dilipraj10 <dilipraj5655@gmail.com>
2024-05-16 18:48:18 +0530

    spring_boot_exception

--- day12/demo/src/main/java/com/excel/demo/handlerexception/EmployeeExceptionHandler.java
+++ day12/demo/src/main/java/com/excel/demo/handlerexception/EmployeeExceptionHandler.java
@@ -2,9 +2,11 @@ package com.excel.demo.handlerexception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.excel.demo.exception.EmployeeNotFoundException;
 
+@RestControllerAdvice
 public class EmployeeExceptionHandler {
 	
 	@ExceptionHandler(EmployeeNotFoundException.class)
